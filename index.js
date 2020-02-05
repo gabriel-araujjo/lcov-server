891514f30b974bee7f448213e23dc9b3098f7884
Gabriel Araújo <gabriel140492@gmail.com>
2020-02-05 02:13:36 -0300

    Implement coverage save

--- index.js
+++ index.js
@@ -1,142 +1,133 @@
-const mongoose = require('mongoose');
 
-mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGO_URL, {}, function(error) {
-  if(error) {
-    console.error(error.message); // eslint-disable-line
-    process.exit(1);
-  }
-});
-
+require('dotenv').config()
 const express = require('express');
-const Badge = require('badgeit');
-const parse = require('git-url-parse');
+// const Badge = require('badgeit');
+// const parse = require('git-url-parse');
 const path = require('path');
 const serveStatic = require('serve-static');
 const compression = require('compression');
 const bodyParser = require('body-parser');
 const zlib = require('zlib');
+const url = require('url');
 
 zlib.level = zlib.Z_BEST_COMPRESSION;
 
 const Coverage = require('./lib/coverage');
 
 const port = process.env.PORT || 8080;
-const asyncMiddleware = (fn) => {
-	return (req, res, next) => {
-		Promise.resolve(fn(req, res, next)).catch(next);
-	};
-};
 const app = express();
 
 app.use(bodyParser.json({limit: '100MB'}));
 app.use(compression());
-app.use(serveStatic(path.resolve(__dirname, 'dist'), {
-  maxAge: '1d',
-  etag: false
-}));
+app.use(serveStatic(path.resolve(__dirname, 'dist')));
 
-app.post('/api/upload', asyncMiddleware(async (req, res) => {
-  let { git, run_at, source_files, service_job_id, service_pull_request, service_name } = req.body;
-
-  // Make sure the remote url is set correctly
-  git.remotes.url = parse(parse(git.remotes.url).toString("ssh")).toString("https");
-
-  try {
-    const results = await Coverage.save({
-        source_files,
-        git,
-        run_at,
-        service_job_id,
-        service_pull_request,
-        service_name
-    });
-    res.send({ results });
-  } catch(error) {
-    res.status(500);
-    res.send({ error });
-  }
-}));
-
-app.get('/api/repos', asyncMiddleware(async (req, res) => {
-  try {
-    const repos = await Coverage.repos();
-    res.send(repos);
-  } catch(error) {
-    res.status(500);
-    res.send({ error });
-  }
-}));
-
-app.get('/api/feed/:page?', asyncMiddleware(async (req, res) => {
-  const { page } = req.params;
-
-  try {
-    const feed = await Coverage.feed(page);
-    res.send(feed);
-  } catch(error) {
-    res.status(500);
-    res.send({ error });
-  }
-}));
-
-app.get('/api/repos/:service/:owner/', asyncMiddleware(async (req, res) => {
-  const { service, owner } = req.params;
-
-  try {
-    const coverages = await Coverage.repos(new RegExp(`${service.replace(/%2E/g, '.')}.*/${owner}/`));
-    res.send(coverages);
-  } catch(error) {
-    res.status(500);
-    res.send({ error });
-  }
-}));
-
-app.get('/api/coverage/:service/:owner/:repo/', asyncMiddleware(async (req, res) => {
-  const { limit } = req.query;
-  const { service, owner, repo } = req.params;
-
-  try {
-    const coverages = await Coverage.get(new RegExp(`${service.replace(/%2E/g, '.')}.*/${owner}/${repo}`), limit);
-    res.send(coverages);
-  } catch(error) {
-    res.status(500);
-    res.send({ error });
-  }
-}));
-
-app.get('/badge/:service/:owner/:repo.svg', asyncMiddleware(async (req, res) => {
-  const { service, owner, repo } = req.params;
-
-  try {
-    const coverages = await Coverage.get(new RegExp(`${service.replace(/%2E/g, '.')}.*/${owner}/${repo}`), 1);
-    const coverage = coverages[0];
-    const { history } = coverage;
-    const { source_files } = history[0];
-    let found = 0;
-    let hit = 0;
-    source_files.forEach((file) => {
-      const { lines={hit: 0, found: 0} } = file;
-      found += lines.found;
-      hit += lines.hit;
-    });
-    const percentage = parseInt((hit / found) * 100);
-    const color = percentage >= 85 ? '#3DB712' : percentage <= 85 && percentage >= 70 ? '#caa300' : '#cc5338';
-
-    const badge = await Badge({ color: { right: color }, text: ['coverage', `${percentage}%`] });
-    res.set('Content-Type', 'image/svg+xml; charset=utf-8');
-    res.send(badge);
-  } catch(error) {
-    const badge = await Badge({ color: { right: "#b63b3b" }, text: ['coverage', 'not found'] });
-    res.set('Content-Type', 'image/svg+xml; charset=utf-8');
-    res.send(badge);
-  }
-}));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
+app.post('/api/upload', (req, res, next) => {
+  Coverage.save(req.body)
+      .then(([rep, com]) => {
+        const pathname = '/api/report';
+        const location = url.format({query: {rep, com}, pathname});
+        res.setHeader('Location', location)
+        res.status(201).end();
+      })
+      .catch(next);
 });
 
-app.listen(port, function () {
-  console.log(`lcov-server is listening on http://localhost:${port}`); // eslint-disable-line
-});
+// app.get('/api/report')
+
+// app.get('/api/repos', asyncMiddleware(async (req, res) => {
+//           try {
+//             const repos = await Coverage.repos();
+//             res.send(repos);
+//           } catch (error) {
+//             res.status(500);
+//             res.send({error});
+//           }
+//         }));
+
+// app.get('/api/feed/:page?', asyncMiddleware(async (req, res) => {
+//           const {page} = req.params;
+
+//           try {
+//             const feed = await Coverage.feed(page);
+//             res.send(feed);
+//           } catch (error) {
+//             res.status(500);
+//             res.send({error});
+//           }
+//         }));
+
+// app.get('/api/repos/:service/:owner/', asyncMiddleware(async (req, res) => {
+//           const {service, owner} = req.params;
+
+//           try {
+//             const coverages = await Coverage.repos(
+//                 new RegExp(`${service.replace(/%2E/g, '.')}.*/${owner}/`));
+//             res.send(coverages);
+//           } catch (error) {
+//             res.status(500);
+//             res.send({error});
+//           }
+//         }));
+
+// app.get(
+//     '/api/coverage/:service/:owner/:repo/',
+//     asyncMiddleware(async (req, res) => {
+//       const {limit} = req.query;
+//       const {service, owner, repo} = req.params;
+
+//       try {
+//         const coverages = await Coverage.get(
+//             new RegExp(`${service.replace(/%2E/g, '.')}.*/${owner}/${repo}`),
+//             limit);
+//         res.send(coverages);
+//       } catch (error) {
+//         res.status(500);
+//         res.send({error});
+//       }
+//     }));
+
+// app.get(
+//     '/badge/:service/:owner/:repo.svg', asyncMiddleware(async (req, res) => {
+//       const {service, owner, repo} = req.params;
+
+//       try {
+//         const coverages = await Coverage.get(
+//             new RegExp(`${service.replace(/%2E/g, '.')}.*/${owner}/${repo}`),
+//             1);
+//         const coverage = coverages[0];
+//         const {history} = coverage;
+//         const {source_files} = history[0];
+//         let found = 0;
+//         let hit = 0;
+//         source_files.forEach((file) => {
+//           const {
+//             lines =
+//             {
+//               hit: 0, found: 0
+//             }
+//           } = file;
+//           found += lines.found;
+//           hit += lines.hit;
+//         });
+//         const percentage = parseInt((hit / found) * 100);
+//         const color = percentage >= 85 ?
+//             '#3DB712' :
+//             percentage <= 85 && percentage >= 70 ? '#caa300' : '#cc5338';
+
+//         const badge = await Badge(
+//             {color: {right: color}, text: ['coverage', `${percentage}%`]});
+//         res.set('Content-Type', 'image/svg+xml; charset=utf-8');
+//         res.send(badge);
+//       } catch (error) {
+//         const badge = await Badge(
+//             {color: {right: '#b63b3b'}, text: ['coverage', 'not found']});
+//         res.set('Content-Type', 'image/svg+xml; charset=utf-8');
+//         res.send(badge);
+//       }
+//     }));
+
+// app.get('*', (req, res) => {
+//   res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
+// });
+
+app.listen(port, () => console.log(`listening on http://localhost:${port}`));
--- lib/migrations/1.sql
+++ lib/migrations/1.sql
@@ -1,43 +1,60 @@
-CREATE TABLE IF NOT EXISTS project (
+--create citype
+CREATE TYPE citype AS ENUM (
+    'travis-ci',
+    'drone',
+    'jenkins',
+    'circleci',
+    'codeship',
+    'wercker',
+    'gitlab-ci',
+    'appveyor',
+    'surf'
+);
+
+CREATE TABLE project (
     gid bigint PRIMARY KEY,
     rep text NOT NULL UNIQUE -- repository ex. example/example
 );
 
-CREATE TABLE IF NOT EXISTS report (
+CREATE TABLE report (
     gid bigint PRIMARY KEY,
     pid bigint NOT NULL REFERENCES project (gid), -- project gid
     com char(40) NOT NULL, -- commit hash
     bra text NOT NULL, -- branch name
     rat timestamp NOT NULL, -- run at
+    ci citype, -- type of ci
+    job text, -- job
+    prq text, -- pull request or merge request id
     bct bigint NOT NULL, -- total block counts in this report
     bex bigint NOT NULL, -- total blocks executed int this report
     CONSTRAINT uinique_commit UNIQUE (pid, com)
 );
 
-CREATE TABLE IF NOT EXISTS file (
+CREATE TABLE file (
     gid bigint PRIMARY KEY,
     pid bigint REFERENCES project (gid),
     pat text NOT NULL, -- relative path (ex. main.cc, src/lib.cc)
     CONSTRAINT unique_path UNIQUE (pid, pat)
 );
 
-CREATE TABLE IF NOT EXISTS line_cov (
+CREATE TABLE line_cov (
     fid bigint NOT NULL REFERENCES file (gid),
     rid bigint NOT NULL REFERENCES report (gid),
     lno smallint NOT NULL, -- line number - starts at zero
     hit smallint NOT NULL, -- how many times the line was executed
-    bct smallint NOT NULL, -- branch count
-    bex smallint NOT NULL, -- branches executed
+    bct smallint, -- branch count
+    bex smallint, -- branches executed
     PRIMARY KEY (fid, rid, lno)
 );
 
-CREATE TABLE IF NOT EXISTS func_cov (
+CREATE TABLE func_cov (
     fid bigint NOT NULL REFERENCES file (gid),
     rid bigint NOT NULL REFERENCES report (gid),
-    lno smallint NOT NULL, -- line where function is, starts at zero
+    sln smallint, -- line where function starts, starts at zero
+    eln smallint, -- line where function ends, inclusive
     fna text NOT NULL, -- function name
     hit int NOT NULL, -- execution count
-    bct smallint NOT NULL, -- number of blocks in function
-    bex smallint NOT NULL, -- how many block were executed
-    CONSTRAINT unique_fn UNIQUE (fid, rid, lno, fna)
+    bct smallint, -- number of blocks in function
+    bex smallint, -- how many block were executed
+    CONSTRAINT unique_fn UNIQUE (fid, rid, sln, fna)
 );
