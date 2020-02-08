2f272f4ff9d255e07c391926085625631300976e
Gabriel Araújo <gabriel140492@gmail.com>
2020-02-08 01:37:10 -0300

    WIP: Front-end

--- src/router.js
+++ src/router.js
@@ -9,16 +9,25 @@ import Coverage from './coverage/coverage';
 import File from './coverage/file';
 import Error from './components/error';
 
-export default (
+function Dummy({match: {params}}) {
+  console.log(params)
+  return <h1> Dummy </h1>
+}
+
+export default (  
   <BrowserRouter>
     <Layout>
       <Switch>
+        <Route path="/:rep([a-zA-Z0-9\\-_\\.\\/]+)/-/tree/:com/:dir([a-zA-Z0-9\\-_\\.\\/]+)?" component={Dummy} />
+        <Route path="/:rep([a-zA-Z0-9\\-_\\.\\/]+)/-/blob/:com/:dir([a-zA-Z0-9\\-_\\.\\/]+)?" component={Dummy} />
+        {/* <Route path="/:dir/:name/blob/:com/*" component={Dummy} />
+        <Route path="/:dir/:name" component={Dummy} />
         <Route path="/coverage/:source/:owner/:name/:file" component={File} />
         <Route path="/coverage/:source/:owner/:name" component={Coverage} />
         <Route path="/coverage/:source/:owner/:page?" component={List} />
         <Route path="/coverage/:page?" component={List} />
         <Route exact path="/feed" component={Feed} />
-        <Route exact path="/" component={Main} />
+        <Route exact path="/" component={Main} /> */}
         <Route path="*" component={Error} />
       </Switch>
     </Layout>
